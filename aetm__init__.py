bfae112f1a23b14e0182486d1105e80b3ae75309
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-27 00:08:53 +0100

    Automated commit by HiveMind

---
+++ aetm__init__.py
@@ -0,0 +1,6 @@
+"""
+Autonomous Evolutionary Trading Module (AETM)
+Core package for self-evolving trading strategies using deep learning and reinforcement learning.
+"""
+__version__ = "1.0.0"
+__author__ = "Evolution Ecosystem"
